6949d6d913a30a9c8d176e1ae184e2e0a5a00e5b
Karatino <viewkaratsonic@mail.ru>
2022-09-02 21:11:14 +0500

    Finding the maximum value of three numbers

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Получения первого числа от пользователя
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+// Получение второго числа от пользователя
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+// Получение третьего числа от пользователя
+Console.Write("Введите второе число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+// Присвоение переменных
+int max = number1;
+
+// Поиск наибольшего числа
+
+if (number2 > number1)
+    max = number2;
+if (number3 > max)
+    max = number3;
+
+// Вывод на экран
+  Console.WriteLine($"Максимальное число {max}");
