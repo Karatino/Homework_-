47b91118af468c64efde51bd09b3be08efd72d11
Karatino <viewkaratsonic@mail.ru>
2022-09-02 16:38:22 +0500

    Finding the maximum and minimum number

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Получения первого числа от пользователя
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+// Получение второго числа от пользователя
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+// Присвоение переменных
+int max = number1;
+int min = number2;
+
+// Поиск наибольшего числа
+
+  {
+    if (max < min) 
+
+       {
+            max = number2;
+            min = number1;
+       }
+    else
+        {
+            max = number1;
+            min = number2;
+        }
+  }
+
+// Вывод на экран
+  Console.WriteLine($"Максимальное число {max}");
+  Console.WriteLine($"Минимальное число {min}");
